46faac19aad6025346306cc2633b0a18fc56307a
sgarcia <stevensantosgarcia@gmail.com>
2025-03-28 17:24:03 -0500

    Add simple test for Scanner

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import Test.QuickCheck
+--import Test.HUnit
+import qualified Scanner as S
+
+prop_scannerNoErrors :: FilePath -> Property
+prop_scannerNoErrors filePath = ioProperty $ do
+  fileContents <- readFile filePath
+  let results = S.scanTokens fileContents
+  let hasError = any isLeft results
+  if hasError
+    then do
+      putStrLn "Scanner Errors found."
+      return False
+    else do
+      putStrLn "Scanner processed file without errors."
+      return True
+  where
+    isLeft (Left _) = True
+    isLeft (Right _) = False
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do 
+  quickCheckWith (stdArgs { maxSize = 5, maxSuccess = 5 }) (prop_scannerNoErrors "data/parens.lox") 
