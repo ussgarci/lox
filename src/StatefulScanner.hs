cd29983103e0c0e603dcd3a5174974a53012ca53
sgarcia <stevensantosgarcia@gmail.com>
2025-04-09 13:13:28 -0500

    Fix build

--- src/StatefulScanner.hs
+++ src/StatefulScanner.hs
@@ -3,20 +3,22 @@ module StatefulScanner (
 )
 where
 
+import Control.Monad.State
 import Data.Char (isAlpha, isAlphaNum, isDigit)
-import Data.Map (Map)
-import Data.Map qualified as M
+import qualified Data.Map as M
 import Token (Literal (..), Token (..), TokenType (..), TokenPos (..))
 
 type Line = Int
 type Column = Int
 type ScannerState = (Line, Column)
 
+startState :: ScannerState
 startState = (0,0)
 
-scan :: String -> State ScannerState [Either Error Token]
-scan [] = do
-    (ln, cl) <- get
-    return Right $ Token EOF "" Nothing TokenPos "" ln cl
-scan (x:xs) = undefined
+scan :: String -> State ScannerState [Token]
+scan = undefined
+-- scan [] = do
+--     (ln, cl) <- get
+--     return Right $ Token EOF "" Nothing TokenPos "" ln cl
+-- scan (x:xs) = undefined
 
