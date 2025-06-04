49461ba0b29412e105e0e3cab5ed94b5504277e4
sgarcia <stevensantosgarcia@gmail.com>
2025-06-03 20:27:51 -0500

    Nil

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Parser (
-    parse
+    parse,
 )
 where
 
-import Expr
-import StatefulScanner (Token)
 import Control.Monad.State
+import StatefulScanner (Token)
 
 data ParserState = ParserState
     { current :: Int
@@ -13,17 +15,29 @@ data ParserState = ParserState
     }
     deriving (Show)
 
--- expression     → literal
---                | unary
---                | binary
---                | grouping ;
---
--- literal        → NUMBER | STRING | "true" | "false" | "nil" ;
--- grouping       → "(" expression ")" ;
--- unary          → ( "-" | "!" ) expression ;
--- binary         → expression operator expression ;
--- operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
---                | "+"  | "-"  | "*" | "/" ;
+data Expr where
+    Binary :: Expr -> Token -> Expr -> Expr
+    Unary :: Token -> Expr -> Expr
+    Literal :: Token -> Expr
+    Grouping :: Expr -> Expr
+
+deriving instance Show Expr
+
+-- expression     → equality ;
+-- equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+-- comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+-- term           → factor ( ( "-" | "+" ) factor )* ;
+-- factor         → unary ( ( "/" | "*" ) unary )* ;
+-- unary          → ( "!" | "-" ) unary
+--                | primary ;
+-- primary        → NUMBER | STRING | "true" | "false" | "nil"
+--                | "(" expression ")" ;
 
 parse :: State ParserState Expr
 parse = undefined
+
+expression :: State ParserState Expr
+expression = undefined
+
+equality :: State ParserState Expr
+equality = undefined
