95352b9174f0ffbf4f75a592896b26bfa2e5b25f
sgarcia <stevensantosgarcia@gmail.com>
2025-06-27 18:20:04 -0500

    Update repl with optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,19 @@ module Main (main) where
 
 import Control.Monad (unless)
 import Control.Monad.State (execState)
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
-import System.Environment (getArgs)
 import System.IO (isEOF)
 
+import Options.Applicative
+
 import qualified StatefulScanner as SS
+
+data Command
+    = RunScannerREPL
+    | RunParserREPL
+    | RunFile FilePath
+    deriving (Show)
 
 runFile :: FilePath -> IO ()
 runFile fname = do
@@ -18,24 +26,65 @@ scannerPrompt :: IO ()
 scannerPrompt = go
   where
     go = do
+        putStr "> "
         ended <- isEOF
-        Control.Monad.unless
+        unless
             ended
             ( do
                 input <- getLine
-                let result = execState SS.scanTokens (SS.ScannerState (T.pack input) 0 0 1 [] [])
-                print $ SS.tokens result
+                unless (null input) $ do
+                    let result = execState SS.scanTokens (SS.ScannerState (T.pack input) 0 0 1 [] [])
+                    print $ SS.tokens result
                 go
             )
 
 parserPrompt :: IO ()
-parserPrompt = undefined
+parserPrompt = putStrLn "Parser not yet implemented."
+
+scannerP :: Parser Command
+scannerP =
+    flag'
+        RunScannerREPL
+        ( long "scanner"
+            <> short 's'
+            <> help "Run the interactive scanner REPL"
+        )
+
+parserP :: Parser Command
+parserP =
+    flag'
+        RunParserREPL
+        ( long "parser"
+            <> short 'p'
+            <> help "Run the interactive parser REPL"
+        )
+
+fileP :: Parser Command
+fileP =
+    RunFile
+        <$> strArgument
+            ( metavar "FILE"
+                <> help "The lox file to run"
+            )
+
+commandParser :: Parser Command
+commandParser = scannerP <|> parserP <|> fileP
+
+programOptions :: Parser Command
+programOptions = fromMaybe RunScannerREPL <$> optional commandParser
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        ["-s"] -> scannerPrompt
-        ["-p"] -> parserPrompt
-        [fname] -> runFile fname
-        _ -> putStrLn "Usage: lox [script] | -s | -p"
+    selectedCommand <- execParser opts
+    case selectedCommand of
+        RunScannerREPL -> scannerPrompt
+        RunParserREPL -> parserPrompt
+        RunFile path -> runFile path
+  where
+    opts =
+        info
+            (programOptions <**> helper)
+            ( fullDesc
+                <> progDesc "Runs the Lox interpreter. Defaults to an interactive scanner REPL."
+                <> header "lox - A Lox interpreter in Haskell"
+            )
